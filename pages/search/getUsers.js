017c74aad010569fdc49ecacbf3d3674c1d6e325
Jacob Roush <jacobroushmail@gmail.com>
2019-11-02 14:12:24 -0500

    list made

--- pages/search/getUsers.js
+++ pages/search/getUsers.js
@@ -1,8 +1,44 @@
 import fetch from 'cross-fetch'
 
 export const getUsers = async (location, language, experience) => {
-    let response = await fetch('https://api.github.com/search/users?q=language:javascript+location:Iowa');
+
+    let response = await fetch(`https://api.github.com/search/users?q=language:${language}+location:${location}`);
     let data = await response.json();
-    console.log(data)
-    return data;
+
+    // let headers = new Headers()
+    // headers.set('Authorization', 'Token ' + process.env.GH_TOKEN);
+    // console.log(process.env.GH_TOKEN);
+
+    const userIds = data.items.map(user => user.login);
+    if (userIds.length > 5) {
+        userIds.splice(5);
+    }
+    console.log(userIds)
+
+    const rawUserData = await Promise.all(
+        userIds.map(user =>
+            fetch(`https://api.github.com/users/${user}`, {
+                method: 'get',
+                headers: {
+                    'Authorization': `token ${process.env.GH_TOKEN}`
+                }
+            })
+                .then(response => response.json()
+                    .catch(e => console.log(e))
+                )));
+
+    console.log(rawUserData);
+
+    const userData = rawUserData.map(user => ({
+        avatar_url: user.avatar_url,
+        user: user.name,
+        company: user.company,
+        blog: user.blog,
+        location: user.location,
+        hireable: user.hireable,
+        bio: user.bio
+    }))
+    console.log(userData)
+
+    return userData;
 }
--- pages/search/index.js
+++ pages/search/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Head from 'next/head'
 import Nav from '../../components/nav'
 import { getUsers } from './getUsers'
+import UserCard, { userCard } from '../../components/userCard'
 import styled from 'styled-components'
 import InputLabel from '@material-ui/core/InputLabel'
 import Select from '@material-ui/core/Select'
@@ -10,11 +11,31 @@ import TextField from '@material-ui/core/TextField'
 import SimpleMenu from '../../components/dropdown'
 import Button from "@material-ui/core/button"
 
+const CardScrollContainer = styled.div`
+  display: flex;
+  overflow: hidden;
+  height: 100vh;
+  margin-top: -100px;
+  padding-top: 100px;
+  position: relative;
+  width: 100%;
+  backface-visibility: hidden;
+  will-change: overflow;
+`
+
 const Layout = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
   flex-wrap: wrap;
+`
+
+const ScrollSection = styled.div`
+  overflow: auto;
+  height: auto;
+  padding: .5rem;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: none;
 `
 
 const Column = styled.div`
@@ -26,24 +47,35 @@ const Column = styled.div`
   flex: 1;
   flex-direction: column;
   box-sizing:border-box;
+  max-height: 90vh;
+`
+const StyledSelect = styled.div`
+padding-top: 12px;
 `
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { language: '', location: '', experience: '' };
+    this.state = { language: '', location: '', experience: 0, users: [] };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = (e) => {
-    getUsers()
+  handleSubmit = async (e) => {
     e.preventDefault();
+    const results = await getUsers(this.state.location, this.state.location)
+    this.setState({ users: results })
   }
 
   handleChange = field => event => {
     this.setState({ [field]: event.target.value });
+  }
+
+  handleSelect = event => {
+    console.log('selector')
+    this.setState({ experience: event.target.value })
   }
 
   render() {
@@ -53,24 +85,34 @@ class Home extends Component {
           <h1>{"Search for a Developer"}</h1>
           <TextField name="language" label="Programing Language" value={this.state.value} onChange={this.handleChange('language')}></TextField>
           <TextField name="location" label="Location" value={this.state.value} onChange={this.handleChange('location')}></TextField>
-          <InputLabel id="demo-simple-select-label" value={this.state.value} onChange={this.handleChange}>Experience</InputLabel>
+          <StyledSelect>
+            <InputLabel id="demo-simple-select-label" value={this.state.value} onChange={this.handleChange}>Experience</InputLabel>
+          </StyledSelect>
+
           <Select
             labelId="demo-simple-select-helper-label"
             id="demo-simple-select-helper"
-            value={''}
-            onChange={'handleChange'}
+            value={this.state.experience}
+            onChange={this.handleSelect}
           >
-            {/* <MenuItem value="dsfgdfg">
-          <em>None</em>
-        </MenuItem> */}
-            <MenuItem>{'Beginner'}</MenuItem>
-            <MenuItem>{'Intermediate'}</MenuItem>
-            <MenuItem>{'Advanced'}</MenuItem>
+            <MenuItem value="All">
+              {'All'}
+            </MenuItem>
+            <MenuItem value={1}>{'Beginner'}</MenuItem>
+            <MenuItem value={2}>{'Intermediate'}</MenuItem>
+            <MenuItem value={3}>{'Advanced'}</MenuItem>
           </Select>
           <Button label="Submit" type="submit" onClick={this.handleSubmit} >Submit</Button>
         </Column>
         <Column>
           <h1>{"Devs"}</h1>
+          <CardScrollContainer>
+            <ScrollSection>
+              {this.state.users.map((user, index) => (
+                <UserCard {...this.state.users[index]} />
+              ))}
+            </ScrollSection>
+          </CardScrollContainer>
         </Column>
       </Layout>
     )
