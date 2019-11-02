5ac2c08771f03432bf89d5fde1e3f269cd16d3aa
Jonathan <jonathannsegal@gmail.com>
2019-11-02 06:41:31 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import Nav from '../components/nav'
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components'
-import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-const NewText = styled.h1`
-  text-align: center;
-  color: red;
-  background-color: blue;
+const Image = styled.img`
+  height:10em;
 `
 
 const Home = () => (
@@ -18,28 +17,53 @@ const Home = () => (
       <link rel='icon' href='/favicon.ico' />
     </Head>
 
-    <Nav />
-
     <div className='hero'>
       <h1 className='title'>Git a Dev</h1>
       <p className='description'>
         Use the power of Github to find good developers and learn what makes a good profile.
       </p>
 
+      {/* <div className='pageDiv'>
+        hi
+        </div>
+      <div className='pageDivRight'>
+        hi
+        </div> */}
+
+
       <div className='row'>
-        <Link href='/insight' className="card">
-          <NewText>{'Insight'}</NewText>
-        </Link>
         <Link href='/search'>
-          <NewText>{'Search'}</NewText>
+          <Button>
+            <Image src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/people_search_wctu.svg" />
+            <Typography>
+              Search
+                </Typography>
+          </Button>
         </Link>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+
+        <Link href='/insight'>
+          <Button>
+            <Image src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/feedback_h2ft.svg" />
+            <Typography>
+              Insight
+            </Typography>
+          </Button>
+        </Link>
+
       </div>
     </div>
 
     <style jsx>{`
+      .pageDiv {
+        height:200px;
+        width:30%;
+        background-color: green;
+      }
+      .pageDivRight {
+        height:200px;
+        width:30%;
+        background-color: blue;
+      }
       .hero {
         width: 100%;
         color: #333;
@@ -85,7 +109,7 @@ const Home = () => (
         color: #333;
       }
     `}</style>
-  </div>
+  </div >
 )
 
 export default Home
